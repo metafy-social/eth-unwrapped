2db912c8ddbe739b8c797910d92e34d28dbd85d7
Advaita Saha <adv11sphs@gmail.com>
2022-12-30 19:36:18 +0530

    fix: cursor feature added

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,12 @@
 //     console.log('Server started on port 3000');
 // });
 
-import { getTransactionCount } from "./lib/transactions";
+import { getTransactions } from "./lib/transactions";
 
-getTransactionCount("0x377ba90a0f828eaC2E63f956875a69e0c16D4C36").then(([data, error]) => {
+getTransactions("0xB62644Da3BB532FdA71eb7A3B3fd8977492A723D").then(([data, error]) => {
     if (error) {
         console.log(error);
     }
     console.log(data);
+    console.log(data?.result.length);
 });
--- src/lib/transactions.ts
+++ src/lib/transactions.ts
@@ -20,8 +20,22 @@ export async function getTransactions(address: string) : Promise<[TransactionDat
         }
     };
 
+
     try {
         const { data } = await axios.request(options);
+        while(data.cursor !== null) {
+            const { data: nextData } = await axios.request({
+                method: 'GET',
+                url: `${MORALIS_BASE_URL}/${address}?chain=eth&cursor=${data.cursor}`,
+                headers: {
+                    accept: 'application/json', 
+                    'X-API-Key': MORALIS_API_KEY
+                }
+            });
+            data.result = [...data.result, ...nextData.result];
+            data.cursor = nextData.cursor;
+        }
+
         return [data, null];
     } catch (error) {
         if (error instanceof Error) {
