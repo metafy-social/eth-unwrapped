06d30f7e6ceac1a1ba278d02fadf9d5dad7344a0
bartick <bartickmaiti1998@gmail.com>
2022-12-31 11:28:40 +0530

    fix: computing speed of stats

--- src/lib/metaScore.ts
+++ src/lib/metaScore.ts
@@ -21,38 +21,51 @@ import timeAgo from './../utils/time';
 const base = 10000000000000000;
 
 export async function metaScore(address: string) : Promise<[Unwrapped | null, string | null]> {
-    // let metaScore = 0.0;
     let unwrapped: Unwrapped = {
         score: 0.0
     };
 
     let nfts: NFTData | null, _;
-    [nfts , _] = await getCollectedNFTs(address);
+    let tokens: TokenData | null;
+    let contracts: number | null;
+    let transactions: TransactionData | null;
+    let oldestTransaction: Transaction | null;
+    let balance: BalanceData | null;
+    let swap: number | null;
+    let firstTransaction2022: Transaction | null;
+
+    [
+        [nfts, _],
+        [tokens, _],
+        [contracts, _],
+        [transactions, _],
+        [oldestTransaction, _],
+        [balance, _],
+    ] = await Promise.all([
+        getCollectedNFTs(address),
+        getCollectedTokens(address),
+        contractsCreated(address),
+        getTransactions(address),
+        getOldestTransaction(address),
+        getBalance(address),
+    ]);
+
     if(!nfts) return [null, "No NFTs found for this address"];
     unwrapped['score'] += nfts.total*2;
     unwrapped['nfts'] = nfts.total;
-
-    let tokens: TokenData | null;
-    [tokens, _] = await getCollectedTokens(address);
+    
     if(!tokens) return [null, "No tokens found for this address"];
     unwrapped['score'] += tokens.length;
     unwrapped['tokens'] = tokens.length;
-
-    let contracts: number | null;
-    [contracts, _] = await contractsCreated(address);
+    
     if(contracts===null) return [null, "No contracts found for this address"];
     unwrapped['score'] += contracts*3;
     unwrapped['contracts'] = contracts;
-
-    let transactions: TransactionData | null;
-    [transactions, _] = await getTransactions(address);
+    
     if (!transactions) return [null, "No transactions found for this address"];
     unwrapped['score'] += transactions.total;
     unwrapped['transactions'] = transactions.total;
     
-    let oldestTransaction: Transaction | null;
-    [oldestTransaction, _] = await getOldestTransaction(address);
-    // console.log("Oldest ", oldestTransaction?.block_timestamp);
     if (oldestTransaction!==null) {
         const block_timestamp = oldestTransaction.block_timestamp;
         const block_timestamp_date = new Date(block_timestamp);
@@ -68,19 +81,17 @@ export async function metaScore(address: string) : Promise<[Unwrapped | null, st
     } else {
         unwrapped['oldest'] = null;
     }
-
-    let balance: BalanceData | null;
-    [balance, _] = await getBalance(address);
+    
     if (!balance) return [null, "No balance found for this address"];
     unwrapped['score'] += (Number(balance.balance)/base);
 
-    let swap: number | null;
+    
     [swap, _] = await swaps(transactions);
     if (swap===null) return [null, "No swaps found for this address"];
     unwrapped['score'] += swap*1.5;
     unwrapped['swaps'] = swap;
 
-    let firstTransaction2022: Transaction | null;
+    
     [firstTransaction2022, _] = await getFirstTransactionOf2022(transactions);
     if (firstTransaction2022===null) unwrapped['first_transaction_2022'] = null;
     else unwrapped['first_transaction_2022'] = (new Date(firstTransaction2022.block_timestamp)).getTime();
